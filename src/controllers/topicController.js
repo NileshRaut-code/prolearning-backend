642565c993a4404c18c0c42fb3c6eb993f3b010d
Nilesh <nileshraut114@gmail.com>
2024-10-03 15:20:34 +0530

    remove the message

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -217,7 +217,7 @@ export const toggleUpvote = asyncHandler(async (req, res) => {
   await redisClient.del(`comment:${review.topic_id}`)
   return res
     .status(200)
-    .json(new ApiResponse(200, review, `Upvote toggled successfully commentid${review.topic_id}`));
+    .json(new ApiResponse(200, review, "Upvote toggled successfully"));
 });
 export const subtoggleUpvote = asyncHandler(async (req, res) => {
   const { replies_id } = req.params;
